2207d9ac9f86145cbf45e6ce2d054790ff9908d4
Justwouter <wouter2312@gmail.com>
2023-12-10 02:38:31 +0100

    Patrolpoints

--- Gold1AI/Assets/Scripts/Bahaviour/Patrol/PatrolBehaviour.cs
+++ Gold1AI/Assets/Scripts/Bahaviour/Patrol/PatrolBehaviour.cs
@@ -10,7 +10,7 @@ public class PatrolBehaviour : MonoBehaviour {
     [SerializeField] private float restTime;
 
     [Header("Route")]
-    [SerializeField] private Transform[] route;
+    [SerializeField] private PatrolPoint[] route;
 
 
     private int routeIndex = 0;
@@ -22,14 +22,16 @@ public class PatrolBehaviour : MonoBehaviour {
     }
 
     public void Update() {
-        if (transform.position != route[routeIndex].position) {
-            transform.position = Vector3.MoveTowards(transform.position, route[routeIndex].position, speed * Time.deltaTime);
+        Vector3 nextPosition = route[routeIndex].transform.position;
+        if (V3NoY(transform.position) != V3NoY(nextPosition)) {
+            agent.SetDestination(nextPosition);
         }
         else {
             if (!isResting) {
                 StartCoroutine(RestAtPoint());
             }
         }
+
     }
 
     IEnumerator RestAtPoint() {
@@ -44,12 +46,20 @@ public class PatrolBehaviour : MonoBehaviour {
         isResting = false;
     }
 
+    /// <summary>
+    /// Quick and stupid way to ignore hight values when comparing vectors
+    /// </summary>
+    /// <param name="inputVector"></param>
+    /// <returns></returns>
+    private Vector3 V3NoY(Vector3 inputVector){
+        return new Vector3(inputVector.x,0,inputVector.z);
+    }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         for (int i = 0; i < route.Length; i++) {
-            Vector3 start = route[i].position;
-            Vector3 destination = i < route.Length - 1 ? route[i + 1].position : route[0].position;
+            Vector3 start = route[i].transform.position;
+            Vector3 destination = i < route.Length - 1 ? route[i + 1].transform.position : route[0].transform.position;
             Gizmos.DrawLine(start, destination);
         }
     }
---
+++ Gold1AI/Assets/Scripts/Bahaviour/Patrol/PatrolPoint.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PatrolPoint : MonoBehaviour {
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, .3f);
+    }
+}
